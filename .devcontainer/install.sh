cfb563806723f7b2562b55ca410cfdf460fdac5d
Brian Sheridan <brian.sheridan@gmail.com>
2022-06-11 18:16:44 +0000

    Added Dapr Init

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 dotnet tool install -g Microsoft.Tye --version "0.11.0-alpha.22111.1"
 wget -q https://raw.githubusercontent.com/dapr/cli/master/install/install.sh -O - | /bin/bash
+dapr init
